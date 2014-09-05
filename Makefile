d1341e2a867078c8b4d6473cb6b0c7bc9b9ecdac
James Riley Wynne III <wynnejr@ornl.gov>
2014-09-05 14:37:32 -0400

    Accelerated the matrix transpose with CUDA

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ all : matrixMagic.cpp lib
 	$(CC) -arch=sm_35 --shared -g --compiler-options '-fPIC' -c matrixMagic.cpp -o matrixMagic.o
 	$(CC) -arch=sm_35 --shared -g --compiler-options '-fPIC' -c matrixMagic.cu -o cudaMatrixMagic.o
 	$(CC) -arch=sm_35 --shared -o lib/libMatrixMagic.so matrixMagic.o cudaMatrixMagic.o
+	cp lib/libMatrixMagic.so $(MEMBERWORK)/stf007
 
 tests : $(FILENAME)
 
--- matrixMagic.cu
+++ matrixMagic.cu
@@ -2,19 +2,40 @@
 #include "matrixMagic.h"
 using namespace std;
 
-__global__ void transposeKernel(double* internal_storage)
+#define internal_storage(r, c) (internal_storage[(r)*n + (c)])
+#define output(r, c) (output[(r)*n + (c)])
+
+void CUDAErrorCheck()
 {
-  int tid = blockIdx.x*blockDim.x+threadIdx.x;
-  double temp = internal_storage[tid];
-  internal_storage[tid] = internal_storage[threadIdx.x * blockDim.x + blockIdx.x];
-  internal_storage[threadIdx.x * blockDim.x + blockIdx.x] = temp;
+  cudaError_t error = cudaGetLastError();
+  if (error != cudaSuccess)
+  {
+    cout << "CUDA error : " << cudaGetErrorString(error) << " (" << error << ")\n";
+    exit(EXIT_FAILURE);
+  }
+}
+
+__global__ void transposeKernel(double* internal_storage, int m, int n, double* output)
+{
+  int tid = threadIdx.x;
+  for(int j = tid+1; j < n; j++)
+  {
+    double temp = internal_storage(tid,j);
+    internal_storage(tid, j) = internal_storage(j,tid);
+    internal_storage(j,tid) = temp;
+  }
 }
 
 void transposeWrapper(double* internal_storage, int m, int n)
 {
   double* d_storage;
+  double* d_output;
+
   cudaMalloc(&d_storage, m*n*sizeof(double));
+  cudaMalloc(&d_output, m*n*sizeof(double));
   cudaMemcpy(d_storage, internal_storage, m*n*sizeof(double), cudaMemcpyHostToDevice);
-  transposeKernel<<<n,m>>>(d_storage);
+  CUDAErrorCheck();
+  transposeKernel<<<1,m*n>>>(d_storage,m,n,d_output);
   cudaMemcpy(internal_storage, d_storage, m*n*sizeof(double), cudaMemcpyDeviceToHost);
+  CUDAErrorCheck();
 }
